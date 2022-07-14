d3e353c4b3db06cf1bfb5d63d1cdc90ad20c2167
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-14 09:53:21 +0100

    "commit"

--- api/views.py
+++ api/views.py
@@ -7,7 +7,7 @@ def home(request):
     import requests
 
     news_api = requests.get('https://newsapi.org/v2/everything?q=tesla&from=2022-06-14&sortBy=publishedAt&apiKey=7707892e3b0c4cbb837fac5ea740612e')
-    api = news_api.loads(news_api.content)
+    api = json.loads(news_api.content)
     context = {
         'api': api
     }
