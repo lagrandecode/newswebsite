ba2996717868b74ac6741975e490180ba57474b8
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-14 09:08:40 +0100

    "commit"

---
+++ api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    import json
+    import requests
+
+    news_api = requests.get('https://newsapi.org/v2/everything?q=tesla&from=2022-06-14&sortBy=publishedAt&apiKey=7707892e3b0c4cbb837fac5ea740612e')
+    api = news_api.loads(news_api.content)
+    context = {
+        'api': api
+    }
+    return render(request, 'home.html', context)
